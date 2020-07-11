03556c7477c16ee0cc42d85ddbd35c032a0a5c3a
John Sant Anna <john.santannas@gmail.com>
2020-07-11 01:38:27 -0300

    Traços

--- Git/Program.cs
+++ Git/Program.cs
@@ -3,11 +3,13 @@
 namespace Git {
     class Program {
         static void Main(string[] args) {
-            
+
+            Console.WriteLine("---------------------------------");
             Console.WriteLine("OLA MUNDO");
             Console.WriteLine("Como voce está hoje?");
             Console.WriteLine("Tenha um bom dia");
             Console.WriteLine("Tchau");
+            Console.WriteLine("---------------------------------");
 
         }
     }
